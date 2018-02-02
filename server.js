dfbfa6dbf82bb342ad3571cccdac43383965cd91
Erica  Cohen <ericacohen@Ericas-MacBook-Air.local>
2018-02-02 09:34:18 -0800

    Try again

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.use(express.static(path.join(__dirname, "public")));
 // mongoose.Promise = Promise;
 
 if(process.env.MONGODB_URI) {
-    mongoose.connect(process.env.MONGO_URI);
+    mongoose.connect(process.env.MONGODB_URI);
 } else {
     mongoose.connect("mongodb://localhost/project")
 }
